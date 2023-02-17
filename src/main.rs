4dad79264171145906a84a4d1f3a26d365dc3264
Rob McLean <rob.mclean@atco.com>
2023-02-17 15:17:15 -0700

    Added new player logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use bevy::{prelude::*};
+use player::PlayerPlugin;
 mod player;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        // .add_plugin(PlayerPlugin)
+        .add_plugin(PlayerPlugin)
         .run();
 }
--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 use bevy::prelude::*;
 
 pub struct PlayerPlugin;
@@ -6,7 +8,7 @@ const PIG_SPRITE: &str = "pig.png";
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup_sprites);
+        app.add_startup_system(setup_sprites).add_system(add_player).add_event::<AddPlayerEvent>();
     }
 }
 
@@ -32,4 +34,42 @@ fn setup_sprites(
     commands.insert_resource(PlayerMaterials {
         player: texture_atlases.add(player_texture_atlas),
     });
+}
+
+pub struct AddPlayerEvent(pub Gamepad);
+
+fn add_player(
+    mut commands: Commands,
+    mut ev_add_player: EventReader<AddPlayerEvent>,
+    player_materials: Res<PlayerMaterials>,
+) {
+    for _event in ev_add_player.iter() {
+        commands.spawn(PlayerBundle {
+            sprite: SpriteSheetBundle {
+                texture_atlas: player_materials.player.clone(),
+                transform: Transform {
+                    translation: Vec3::new(0., 0., 1.),
+                    scale: Vec3::new(2., 2.0, 1.),
+                    ..default()
+                },
+                ..default()
+            }
+        });
+    }
+}
+
+#[derive(Bundle)]
+pub struct PlayerBundle {
+    // pub gamepad: Gamepad,
+    // pub available_jumps: AvailableJumps,
+    // pub lives: Lives,
+    // pub damage_taken: DamageTaken,
+    // pub speed: Speed,
+    // pub _p: Player,
+
+    pub sprite: SpriteSheetBundle,
+    // pub body: RigidBody,
+    // pub shape: CollisionShape,
+    // pub velocity: Velocity,
+    // pub rotation_constraints: RotationConstraints,
 }
